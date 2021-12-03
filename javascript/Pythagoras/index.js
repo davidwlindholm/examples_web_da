30cebe64003e004998f2dad0f7a6ce46094f2ae2
David W Lindholm <me@davidlindholm.info>
2021-12-03 08:00:31 +0100

    Updated license and modernized code (let, label)

--- javascript/Pythagoras/index.js
+++ javascript/Pythagoras/index.js
@@ -4,9 +4,9 @@
  * Legal: https://www.gnu.org/licenses/agpl-3.0.en.html
  */
 function udregning() {
-  var katete1 = document.getElementById("katete1").value;
-  var katete2 = document.getElementById("katete2").value;
-  var tempHypo = Math.pow(katete1, 2) + Math.pow(katete2, 2);
-  var hypotenuse = Math.sqrt(tempHypo);
+  let katete1 = document.getElementById("katete1").value;
+  let katete2 = document.getElementById("katete2").value;
+  let tempHypo = Math.pow(katete1, 2) + Math.pow(katete2, 2);
+  let hypotenuse = Math.sqrt(tempHypo);
   document.getElementById("resultat").innerHTML = hypotenuse;
 }
